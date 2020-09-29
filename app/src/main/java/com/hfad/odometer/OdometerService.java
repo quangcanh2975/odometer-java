b9530736638cf65238f854877fca0b36aca3e6b6
quangcanh2975 <ubuntultc@gmail.com>
2020-09-29 22:52:55 +0700

    Display distance successfully on old android devices, newer still failed

---
+++ app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -0,0 +1,80 @@
+package com.hfad.odometer;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+public class OdometerService extends Service {
+    private final IBinder binder = new OdometerBinder();
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.v("onLocationChanged", "IN ON LOCATION CHANGE, lat=" + location.getLatitude() + ", lon=" + location.getLongitude());
+
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onProviderDisabled(String arg0) {
+            }
+
+            @Override
+            public void onProviderEnabled(String arg0) {
+            }
+
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle bundle) {
+            }
+        };
+        LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        assert locManager != null;
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listener);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
+    }
+
+    public double getMiles() {
+        return this.distanceInMeters / 1609.344;
+    }
+}
